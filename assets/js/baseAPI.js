437de9e9f321c8cb7a914cd971978da62b335de6
zhaosq <zhaosq6503@163.com>
2022-03-04 10:31:39 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -2,5 +2,24 @@
 $.ajaxPrefilter(function (options) {
   // 在发起真正的ajax 请求之前，统一拼接请求的根路径
   options.url = 'http://www.liulongbin.top:3007' + options.url
-  console.log(options.url);
+  // console.log(options.url);
+
+  // 统一为有权限的接口，设置 headers 请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂载 complete 回调函数
+  options.complete = function (res) {
+    // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+    // console.log(res.responseJSON);
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 1、强制清空 token
+      localStorage.removeItem('token');
+      // 2、强制跳转到 login.html
+      location.href = '/login.html'
+    }
+  }
 })
